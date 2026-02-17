0d5397e277fd398e7cfff4c42824423c91c854db
shivikakapoor <shivikakapoor04@email.com>
2026-02-16 19:58:15 -0800

    made bookshop I still need to add commets

--- src/tests/ca/bcit/comp2522/bookstoresimulation/Main.java
+++ src/tests/ca/bcit/comp2522/bookstoresimulation/Main.java
@@ -11,5 +11,14 @@ import java.util.List;
  */
 class Main
 {
+    public static void main(final String [] args)
+    {
+        final Bookshop bookshop;
 
+        bookshop = new Bookshop();
+
+        bookshop.print_TITLES_W_IT();
+        bookshop.removeTITLES_THE();
+        bookshop.printSorted();
+    }
 }
